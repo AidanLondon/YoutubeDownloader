5b4c648fa2591287a2e0310c88e72d59427ee1d7
Aidan London <londonacm@gmail.com>
2024-05-16 14:10:47 -0300

    Better comments; if/else chain changed to switch; implemented channel download

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils import download_video, download_playlist, download_channel
 
-# get playlist or video choice
-pv = input("(p)laylist or (v)ideo?\n>> ")
+# get playlist/channel/video choice
+urltype = input("(p)laylist, (c)hannel, or (v)ideo?\n>> ")
+urltype = urltype[0].lower() # strip to first letter lowercase
 # get url
 url = str(input("Enter the URL: \n>> "))
 # get output folder
@@ -9,7 +10,11 @@ print("Enter the destination folder (blank for current):")
 destination = str(input(">> ")) or "."
 
 
-if pv[0].lower() == "p":
-    download_playlist(url, destination)
-elif pv[0].lower() == "v":
-    download_video(url, destination)
+# handle logic based on which type was selected
+match urltype:
+    case "p":
+        download_playlist(url, destination)
+    case "v":
+        download_video(url, destination)
+    case "c":
+        download_channel(url, destination)
--- utils.py
+++ utils.py
@@ -2,21 +2,26 @@ from pytube import Playlist, YouTube, Channel
 from pytube.innertube import _default_clients
 import os
 
-_default_clients["ANDROID_MUSIC"] = _default_clients["IOS"]
+# Fixes issue with "mature" videos throwing errors
+_default_clients["ANDROID_MUSIC"] = _default_clients["IOS"] 
 
 def download_mp3(video, destination):
+    ### Downloads a video to a specific destination as an MP3
+    ### @param video - pytube Video object
+    ### @param destination - string, destination folder to save to
     try:
         # get audio stream
         stream = video.streams.filter(only_audio=True).first()
-        # download audio
+        # download audio (usually as mp4)
         out_file = stream.download(output_path=destination)
-        # change file type
+        # change file type to mp3
         base, ext = os.path.splitext(out_file)
         new_file = base + ".mp3"
         os.rename(out_file, new_file)
-        # result
+        # print result
         print(video.title + " has been successfully downloaded!")
     except Exception as e:
+        # alert user of error and move on to next video (if relevant)
         print("!!!!!!!!!! ERROR downloading video: ", video.title)
         print(e)
         print("Continuing to next video...")
@@ -25,6 +30,7 @@ def download_mp3(video, destination):
 def download_video(url, destination):
     # get video from url
     v = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+    # download video as mp3
     download_mp3(v, destination)
 
 
@@ -32,9 +38,12 @@ def download_playlist(url, destination):
     # get playlist from url
     p = Playlist(url)
     for video in p.videos:
+        # download each video in playlist as mp3
         download_mp3(video, destination)
 
 def download_channel(url, destination):
+    # get channel from url
     c = Channel(url)
     for video in c.videos:
+        # download each video from channel as mp3
         download_mp3(video, destination)
